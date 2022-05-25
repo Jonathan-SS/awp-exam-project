42c2dd09d3e863ea2ce42c1af7bd741b72831e28
Jonathan-SS <56107465+Jonathan-SS@users.noreply.github.com>
2022-05-25 22:25:59 +0200

    fix test

--- app/routes/chat/index.jsx
+++ app/routes/chat/index.jsx
@@ -83,7 +83,7 @@ export async function action({ request }) {
       },
       {
         participants: {
-          $elemMatch: { name: participantId },
+          $elemMatch: { userId: participantId },
         },
       },
     ],
